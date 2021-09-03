63f4157a3d763be7b63cde208ab134b5ee2553af
jd-erreape <juandediosherrero@gmail.com>
2021-09-03 17:40:43 +0200

    [Issues/6] [Task] Create our HomeController#index as the root entry point for our App

--- spec/dummy_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'Dummy' do
-  describe 'Dummy spec to test rspec integration (TO BE DELETED)' do
-    it 'works' do
-      expect(true).to be_truthy
-    end
-  end
-end
