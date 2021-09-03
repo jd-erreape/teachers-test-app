a7bf022df3b06205d264100d3d1bf83b9c5cd260
jd-erreape <juandediosherrero@gmail.com>
2021-09-03 19:53:55 +0200

    [Issues/11] [Task] Integrate Rubocop for styling consistency

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require_relative '../constants/emails'
 
 RSpec.describe Teacher, type: :model do
-  it { should have_secure_password }
-  it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email).case_insensitive }
-  it { should allow_values(*VALID_EMAILS).for(:email) }
-  it { should_not allow_values(*INVALID_EMAILS).for(:email)}
+  it { is_expected.to have_secure_password }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to allow_values(*VALID_EMAILS).for(:email) }
+  it { is_expected.not_to allow_values(*INVALID_EMAILS).for(:email) }
 end
