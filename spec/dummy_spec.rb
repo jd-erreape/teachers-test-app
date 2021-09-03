ff491d272f2ca81426665177c0cfa53bf41b0c91
jd-erreape <juandediosherrero@gmail.com>
2021-09-03 16:57:34 +0200

    [Issues/3] [Task] Install rspec as testing frameword and configure CI to run the specs automatically on PRs and Merges to Master

---
+++ spec/dummy_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Dummy' do
+  describe 'Dummy spec to test rspec integration (TO BE DELETED)' do
+    it 'works' do
+      expect(true).to be_truthy
+    end
+  end
+end
