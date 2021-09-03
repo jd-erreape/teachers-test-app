13bf818386146577093b686f528997a68aa47b11
jd-erreape <juandediosherrero@gmail.com>
2021-09-03 18:24:55 +0200

    [Task] Install Bootstrap in order to add simple styling for our app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+// Bootstrap imports
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+require("stylesheets/application.scss")
 
 Rails.start()
 Turbolinks.start()
